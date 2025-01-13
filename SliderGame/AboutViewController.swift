463abb52e5de6ba482dbde8c6f8c65719651175e
sprysiazhnyi <sprysiazhnyi@competo.io>
2025-01-13 19:07:28 +0200

    add-info

---
+++ SliderGame/AboutViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AboutViewController.swift
+//  SliderGame
+//
+//  Created by Serhii Prysiazhnyi on 13.01.2025.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    @IBAction func closeButtonTapped(_ sender: Any) {
+        dismiss(animated: true)
+    }
+
+}
